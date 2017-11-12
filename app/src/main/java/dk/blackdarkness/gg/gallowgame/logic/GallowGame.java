ebf32f55e7d11d421746e33c6c36e1f618551442
Anders Wiberg Olsen <anders@wiberg.tech>
2017-11-12 18:02:35 +0100

    Updated getWordFromDR

--- app/src/main/java/dk/blackdarkness/gg/gallowgame/logic/GallowGame.java
+++ app/src/main/java/dk/blackdarkness/gg/gallowgame/logic/GallowGame.java
@@ -149,20 +149,26 @@ public class GallowGame {
     }
 
     public void getWordFromDR() throws IOException {
-        String data = getUrl("https://dr.dk");
-        //System.out.println("data = " + data);
+        String data = getUrl("https://dr.dk");;
 
         data = data.substring(data.indexOf("<body")). // fjern headere
                 replaceAll("<.+?>", " ").toLowerCase(). // fjern tags
+                replaceAll("&#198;", "æ"). // erstat HTML-tegn
+                replaceAll("&#230;", "æ"). // erstat HTML-tegn
+                replaceAll("&#216;", "ø"). // erstat HTML-tegn
+                replaceAll("&#248;", "ø"). // erstat HTML-tegn
+                replaceAll("&oslash;", "ø"). // erstat HTML-tegn
+                replaceAll("&#229;", "å"). // erstat HTML-tegn
                 replaceAll("[^a-zæøå]", " "). // fjern tegn der ikke er bogstaver
                 replaceAll(" [a-zæøå] "," "). // fjern 1-bogstavsord
                 replaceAll(" [a-zæøå][a-zæøå] "," "); // fjern 2-bogstavsord
 
         System.out.println("data = " + data);
+        System.out.println("data = " + Arrays.asList(data.split("\\s+")));
         possibleWords.clear();
         possibleWords.addAll(new HashSet<String>(Arrays.asList(data.split(" "))));
 
-        System.out.println("possibleWords = " + possibleWords);
+        System.out.println("muligeOrd = " + possibleWords);
         reset();
     }
 
